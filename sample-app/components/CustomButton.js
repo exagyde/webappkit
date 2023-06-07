bf64ae4b0097ae48ca6ecb061a3ee4af2c444cc9
exagyde <nolann.morence@hotmail.fr>
2023-06-07 23:03:05 +0200

    Init first sample app

---
+++ sample-app/components/CustomButton.js
@@ -0,0 +1,25 @@
+import { CustomComponent } from "../app/App.js";
+
+const STYLE = `
+    color: white;
+    border: 0;
+    outline: none;
+`;
+
+class CustomButton extends CustomComponent {
+    constructor() {
+        super();
+        this.onclick = () => window.location.href = "https://github.com";
+    }
+
+    render() {
+        return `
+            <button style="${STYLE}" class="my-button">${this.properties.value}</button>
+        `;
+    }
+}
+
+export {
+    CustomButton
+};
+
